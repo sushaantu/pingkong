c8118ae15e60de9c740f0c09da7c162ce070f575
Sushaantu <sushaantum@gmail.com>
2015-10-08 03:37:20 -0300

    initial commit

---
+++ docs.php
@@ -0,0 +1,129 @@
+<?php include 'header.php';?>
+
+<style>
+body {
+	background: #fff;
+	color: #111;
+}
+
+.navbar-default {
+	background: -webkit-radial-gradient(closest-corner,rgba(16,47,70,0) 60%,rgba(16,47,70,0.26)),-webkit-linear-gradient(108deg,#26d0ce,#1a2980 90%); 
+}
+</style>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-3" id="leftCol">
+			<div class="well"> 
+      	<ul class="nav nav-stacked" id="sidebar">
+          <li><a href="#sec1">Installation</a></li>
+          <li><a href="#sec2">Section 2</a></li>
+          <li><a href="#sec3">Section 3</a></li>
+          <li><a href="#sec4">Section 4</a></li>
+      	</ul>
+			</div>
+		</div>  
+
+		<div class="col-md-9">
+    	<h2 id="sec0">Installation</h2>
+      At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
+    	find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.
+    	
+  
+ 
+    	<hr>
+    	Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+      eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+      sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.              
+    	Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+      eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+      sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
+    	
+    
+    
+    	<h2 id="sec1">Content</h2>
+    	<p>
+      Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.
+    	</p>
+    	<div class="row">
+        <div class="col-md-6">
+          <div class="panel panel-default">
+            <div class="panel-heading"><h3>Hello.</h3></div>
+            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+            Aliquam in felis sit amet augue.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default">
+            <div class="panel-heading"><h3>Hello.</h3></div>
+            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+            Aliquam in felis sit amet augue.
+            </div>
+          </div>
+        </div>  
+    	</div>
+    
+    	<hr>
+    
+    	<h2 id="sec2">Section 2</h2>
+    	<p>
+      Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+      eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+      sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
+    	</p>
+    	<div class="row">
+    		<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
+        	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
+        	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
+    	</div>
+    
+    	<hr>
+    
+    	<h2 id="sec3">Section 3</h2>
+			Images are responsive sed @mdo but sum are more fun peratis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+      eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+      sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..
+    	<br>
+      Fos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+      sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..
+    
+    
+    	<h2 id="sec4">Section 4</h2>
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+      totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+      eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+      sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+    
+    
+    	<hr>
+  	
+  	
+		</div> 
+	</div>
+</div>
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include 'header.php';?>
+
+
+<div id="hanuman">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text-center">
+          <h1>Website monitoring <em>for</em> sysadmins</h1>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <div id="terminal">
+          <div class="terminal-header">
+            <div class="t-buttons">
+              <button class="t-close"></button>
+              <button class="t-minimize"></button>
+              <button class="t-maximize"></button>
+            </div>
+            <span class="t-title">1. pingkong</span>
+          </div>
+          <main class="prompt">
+            <p>The future ain't what it used to be</p>
+            <p>It's so crowded, nobody comes here anymore</p>
+            <p>I really didn't say everything I said</p>
+          </main>
+          
+        </div>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>  
+</div>
+
+
+
+
+<?php include 'footer.php';?>
