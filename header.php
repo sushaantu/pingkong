8c25eca8d6cb6da7d8d4dc1c393443a21d8d475e
Sushaantu <sushaantum@gmail.com>
2015-12-23 10:40:38 -0300

    removed fontdeck

--- header.php
+++ header.php
@@ -20,25 +20,6 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-  <script type="text/javascript">
-    var html = document.getElementsByTagName('html')[0];
-    html.className += '  wf-loading';
-    setTimeout(function() {
-      html.className = html.className.replace(' wf-loading', '');
-    }, 5000);
-
-    WebFontConfig = { fontdeck: { id: '46804' } };
-
-    (function() {
-      var wf = document.createElement('script');
-      wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-      wf.type = 'text/javascript';
-      wf.async = 'true';
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(wf, s);
-    })();
-	</script>
   </head>
   <body>
   
