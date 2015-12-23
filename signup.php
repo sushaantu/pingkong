e5379f60d8c529e50f9c5bc0b3583c75d4271147
Sushaantu <sushaantum@gmail.com>
2015-12-23 00:59:13 -0300

    welcome page and signup page

--- signup.php
+++ signup.php
@@ -6,25 +6,28 @@
 	}
 </style>
 
-<div class="auth-container">
 
-  <form class="form-signin">
-		<button type="button" class="btn btn-lg btn-default btn-block">Register with Github</button>
-		<hr>
-		<p class="text-muted">or Register with email</p>
-    <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <div class="checkbox">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  </form>
+<div class="minimal">
+	<div class="container">
 
-</div> <!-- /container -->
+	  <form class="form-signin">
+			<button type="button" class="btn btn-lg btn-default btn-block">Register with Github</button>
+			<hr>
+			<p class="text-muted">or Register with email</p>
+	    <label for="inputEmail" class="sr-only">Email address</label>
+	    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+	    <label for="inputPassword" class="sr-only">Password</label>
+	    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+	    <div class="checkbox">
+	      <label>
+	        <input type="checkbox" value="remember-me"> Remember me
+	      </label>
+	    </div>
+	    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	  </form>
+
+	</div> <!-- /container -->
+</div>
 
 
 <?php include 'footer.php';?>
--- welcome.php
+++ welcome.php
@@ -1,31 +1,38 @@
 <?php include 'header.php';?>
 
-<div class="auth-container">
+<style>
+  footer, .navbar-right {
+    display: none;
+  }
+</style>
 
-  <div class="row">
-    <div class="col-md-12">
-      <h1>Welcome!</h1>
+
+<div class="minimal">
+  <div class="container">
+
+    <div class="row">
+      <h1 class="text-center">Welcome!</h1>
     </div>
-  </div>
 
-  <div class="cta">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <div class="clearfix">
-            <p class="pull-left">Get started now</p>
-          </div>
-          <pre><code>curl https://pingkong.io/cli | bash</code></pre>
-          <div class="clearfix">
-            <p class="pull-right"><a href="/docs.php">Refer documentation</a></p>
+    <div class="cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="clearfix">
+              <p class="pull-left">Get started now</p>
+            </div>
+            <pre><code>curl https://pingkong.io/cli | bash</code></pre>
+            <div class="clearfix">
+              <p class="pull-right"><a href="/docs.php">Refer documentation</a></p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
 
-</div> <!-- /container -->
+  </div> <!-- /container -->
+</div>
 
 
 <?php include 'footer.php';?>
