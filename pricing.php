529c20bf8b5423153638a51c98ad981e2c8c55d8
Sushaantu <sushaantum@gmail.com>
2015-12-24 15:34:30 -0300

    test pricing page

---
+++ pricing.php
@@ -0,0 +1,21 @@
+<?php include 'header.php';?>
+
+<style>
+  footer, .navbar-right {
+    display: none;
+  }
+</style>
+
+
+<div class="minimal">
+  <div class="container">
+
+    <div class="row">
+      <h1 class="text-center">Pricing!</h1>
+    </div>
+
+  </div> <!-- /container -->
+</div>
+
+
+<?php include 'footer.php';?>
