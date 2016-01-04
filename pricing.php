40736d04abe7c78c5df1bf0b759c31754013bd2c
Sushaantu <sushaantum@gmail.com>
2016-01-04 15:09:33 -0300

    pricing

--- pricing.php
+++ pricing.php
@@ -4,61 +4,6 @@
   footer, .navbar-right {
     display: none;
   }
-  .pricing .pricing-card {
-		border-radius: 4px;
-  	color: #fff;
-  	padding: 15px 0 20px 0; 
-  }
-
-  .pricing-card h1 {
-  	font-size: 75px;
-  	padding: 15px;
-  }
-
-  .pricing-card h1:before {
-  	content: "$";
-  	font-size: 20px;
-  	top: 7px;
-	  position: relative;
-	  vertical-align: text-top;
-  }
-
-  .pricing-card .btn-default {
-  	background: transparent;
-	  color: #fff;
-	  border: 2px solid #fff;
-	  text-shadow: none;
-  }
-
-  .pricing-card .btn-default:active:focus, .pricing-card .btn-default:active:hover {
-  	color: #fff;
-	  background-color: transparent;
-	  border-color: #fff;
-  }
-
-  .free {
-  	background: #E91E63;
-  }
-
-  .basic {
-  	background: #9C27B0;
-  }
-
-  .plus {
-  	background: #673AB7;
-  }
-
-  .enterprise {
-  	background: #2196F3;
-  }
-
-  .enterprise h1:after {
-		content: "Starts at";
-	  position: absolute;
-	  font-size: 13px;
-	  top: 64px;
-	  left: 100px;
-  }
 </style>
 
 
@@ -91,10 +36,10 @@
 	    		<h1>8</h1>
 	    		<p>15 Checks</p>
 	    		<p>Multiple Zones</p>
-	    		<p>5 minute interval</p>
-	    		<p>7 days</p>
-	    		<p>1 user</p>
-	    		<p>Email</p>
+	    		<p>1 minute interval</p>
+	    		<p>30 days</p>
+	    		<p>3 user</p>
+	    		<p>Email, SMS</p>
 	    		<a class="btn btn-default">Sign up</a>
 	    	</div>
     	</div>
@@ -105,10 +50,10 @@
 	    		<h1>60</h1>
 	    		<p>50 Checks</p>
 	    		<p>Multiple Zones</p>
-	    		<p>5 minute interval</p>
-	    		<p>7 days</p>
-	    		<p>1 user</p>
-	    		<p>Email</p>
+	    		<p>Less than 1 minute interval</p>
+	    		<p>6 months</p>
+	    		<p>Unlimited</p>
+	    		<p>Email, SMS, Phone</p>
 	    		<a class="btn btn-default">Sign up</a>
 	    	</div>
     	</div>
@@ -119,10 +64,10 @@
 	    		<h1>250</h1>
 	    		<p>Unlimited Checks</p>
 	    		<p>Multiple Zones</p>
-	    		<p>5 minute interval</p>
-	    		<p>7 days</p>
-	    		<p>1 user</p>
-	    		<p>Email</p>
+	    		<p>Configurable interval</p>
+	    		<p>Unlimited</p>
+	    		<p>Unlimited</p>
+	    		<p>Email, SMS, Phone</p>
 	    		<a class="btn btn-default">Contact</a>
 	    	</div>
     	</div>
