0371574ff9fe5e6af47103024739c4ece02cb3bd
Sushaantu <sushaantum@gmail.com>
2016-01-05 17:33:36 -0300

    Removed jspark

--- footer.php
+++ footer.php
@@ -20,7 +20,6 @@
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="js/bootstrap.min.js"></script>
   <script src="js/bounce.js"></script>
-  <script src="js/jspark.js"></script>
   <script src="js/typed.min.js"></script>
   <script src="js/theme.js"></script>
   </body>
